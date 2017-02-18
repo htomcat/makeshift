c3cb0262ba353a1dbf25e33ae43214ec52196889
Tomofumi Hamada- <thamada@yahoo-corp.jp>
2017-02-18 20:31:01 +0900

    Initial commit

---
+++ Makeshift/Makeshift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Makeshift
+//
+//  Created by 浜田　智史 on 2017/02/18.
+//  Copyright © 2017年 thamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
